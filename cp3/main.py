bb19ceaebfe15cf20f6023e6dd01bd8ecd2373b5
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-04-17 01:10:44 +0300

    Cp3 | add python list part

--- cp3/main.py
+++ cp3/main.py
@@ -7,10 +7,12 @@ import sys
 L = List()
 test_text = generate_right_text.get_random_text(10_000)
 
-def submit():
+
+def submit_linkedlist():
+    print("Doubly linked list\n------------------")
     print(sys.getsizeof(L))
     start_time = time.time()
-    L.split_text_into_list( test_text )#entry.get())
+    L.split_text_into_list(test_text)  # entry.get())
     print(sys.getsizeof(L))
 
     if L.check_text():
@@ -21,10 +23,33 @@ def submit():
         label_result.config(text="Текст має не коректно введене слово")
 
 
+def submit_pythonlist():
+    print("Python list\n-----------")
+    split_text = []
+    print(sys.getsizeof(split_text))
+    start_time = time.time()
+    text = test_text#entry.get()
+    split_text = text.split()
+    print(sys.getsizeof(split_text))
+    for word in split_text:
+        if 0 == len(word) % 2:
+            label_result.config(text="Текст має не коректно введене слово")
+            return "error"
+    for i in range(len(split_text)):
+        if len(split_text[i]) == 1:
+            split_text[i] = " "
+        else:
+            split_text[i] = split_text[i][1:-1:]
+    print(f"{time.time() - start_time}c")
+    label_result.config(text=str(split_text)[1:-1:].replace("'", ""))
+
+
 root = tk.Tk()
 root.title("DoublyLinkedListCP")
 
-label = tk.Label(root, text="Введіть текст, всі слова якого мають неапрну довжину\n(передбачається, що слова будуть розділені ' '[пробілом]):\n Результат:")
+label = tk.Label(root,
+                 text="Введіть текст, всі слова якого мають неапрну довжину\n(передбачається, що слова будуть "
+                      "розділені ' '[пробілом]):\n Результат:")
 label.pack()
 
 label_result = tk.Label(root, text="")
@@ -33,7 +58,16 @@ label_result.pack()
 entry = tk.Entry(root)
 entry.pack()
 
-button = tk.Button(root, text="Надрукувати слова без першого та останнього\nсимволу і додати до кожного слова знак ','", command=submit)
-button.pack()
+button_linkedlist = tk.Button(root,
+                              text="Надрукувати слова без першого та останнього\nсимволу і додати до кожного слова "
+                                   "знак ','",
+                              command=submit_linkedlist)
+button_linkedlist.pack()
+
+button_pythonlist = tk.Button(root,
+                              text="Надрукувати те саме, що робить і кнопка вище,\nале використовуючи звичайний "
+                                   "пайтоновський список",
+                              command=submit_pythonlist)
+button_pythonlist.pack()
 
 root.mainloop()
